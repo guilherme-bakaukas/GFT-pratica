36991fcbc42abf5bc3f519172c2231e69b50b3a2
guilherme-bakaukas <61765419+guilherme-bakaukas@users.noreply.github.com>
2021-03-04 15:32:55 -0300

    exercício 4 finalizado

---
+++ Exercicios_gft/src/ex4/Main.java
@@ -0,0 +1,20 @@
+package ex4;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		/*observações:
+		Há a possibilidade de manter maiúsculo apenas o primeiro "incomoda" adicionando um if para a primeira iteração
+		no entanto seguindo o exemplo dado, mantive o maiúsculo;
+		valores negativos nessa aplicação funcionariam como o valor nulo.
+		*/
+		
+		Scanner entrada = new Scanner(System.in);
+		int num = entrada.nextInt();
+		
+		for (int i = 0; i < num; i++) {
+			System.out.print("Incomoda ");
+		}
+		System.out.println("muito mais");
+	}
+}
